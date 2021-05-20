210519f4f2da8133b17f7130ca2c5d3fa737e46a
Rutul Maskar <rutulmaskar23@gmail.com>
2021-05-21 00:10:32 +0200

    button name changed to period

--- src/component/CategoryButtons.js
+++ src/component/CategoryButtons.js
@@ -25,7 +25,7 @@ const images = [
 
   {
     url: 'https://images.unsplash.com/photo-1607185073253-44296286cd82?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-    title: 'Sustainable Period',
+    title: 'Period',
     width: '25%',
   },
 
